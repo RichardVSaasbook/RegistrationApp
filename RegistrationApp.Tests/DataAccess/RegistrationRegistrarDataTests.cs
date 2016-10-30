b68051cb72d85eafcaa5ee07f657d1a1a5d5ef3d
Richard Voelker <richard.voelker.software@gmail.com>
2016-10-30 12:26:05 -0400

    Add functionality for listing open and full courses.

---
+++ RegistrationApp.Tests/DataAccess/RegistrationRegistrarDataTests.cs
@@ -0,0 +1,94 @@
+﻿using RegistrationApp.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RegistrationApp.Tests.DataAccess
+{
+    /// <summary>
+    /// Test class for the registrar functionality of RegistrationData.
+    /// </summary>
+    public class RegistrationRegistrarDataTests
+    {
+        /// <summary>
+        /// Make sure we can List all open courses.
+        /// </summary>
+        [Fact]
+        public void Test_ListOpenCourses()
+        {
+            MockDatabase<CourseSchedule> mockDB = new MockDatabase<CourseSchedule>(c => c.CourseSchedules);
+            RegistrationData data = new RegistrationData(mockDB.Context);
+
+            mockDB.AddDataEntry(new CourseSchedule
+            {
+                Capacity = 3,
+                StudentSchedules = new List<StudentSchedule>
+                {
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true }
+                },
+                Course = new Course { Title = "Intro to Computer Science" }
+            });
+
+            mockDB.AddDataEntry(new CourseSchedule
+            {
+                Capacity = 5,
+                StudentSchedules = new List<StudentSchedule>
+                {
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true }
+                },
+                Course = new Course { Title = "Calculus IV" }
+            });
+
+            List<CourseSchedule> courses = data.ListOpenCourses();
+
+            Assert.Equal(1, courses.Count);
+            Assert.Equal("Calculus IV", courses[0].Course.Title);
+        }
+
+        /// <summary>
+        /// Make sure we can List all full courses.
+        /// </summary>
+        [Fact]
+        public void Test_ListFullCourses()
+        {
+            MockDatabase<CourseSchedule> mockDB = new MockDatabase<CourseSchedule>(c => c.CourseSchedules);
+            RegistrationData data = new RegistrationData(mockDB.Context);
+
+            mockDB.AddDataEntry(new CourseSchedule
+            {
+                Capacity = 3,
+                StudentSchedules = new List<StudentSchedule>
+                {
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true }
+                },
+                Course = new Course { Title = "Intro to Computer Science" }
+            });
+
+            mockDB.AddDataEntry(new CourseSchedule
+            {
+                Capacity = 5,
+                StudentSchedules = new List<StudentSchedule>
+                {
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true },
+                    new StudentSchedule { Enrolled = true }
+                },
+                Course = new Course { Title = "Calculus IV" }
+            });
+
+            List<CourseSchedule> courses = data.ListFullCourses();
+
+            Assert.Equal(1, courses.Count);
+            Assert.Equal("Intro to Computer Science", courses[0].Course.Title);
+        }
+    }
+}
