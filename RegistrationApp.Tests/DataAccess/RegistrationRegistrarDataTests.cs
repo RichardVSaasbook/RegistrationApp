c716bc1573a4f78ac5ca0263d5bb84c2e0b0dd1d
Richard Voelker <richard.voelker.software@gmail.com>
2016-10-30 12:31:18 -0400

    Add add and remove student from student body functionality.

--- RegistrationApp.Tests/DataAccess/RegistrationRegistrarDataTests.cs
+++ RegistrationApp.Tests/DataAccess/RegistrationRegistrarDataTests.cs
@@ -1,4 +1,5 @@
-﻿using RegistrationApp.DataAccess;
+﻿using Moq;
+using RegistrationApp.DataAccess;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -89,6 +90,40 @@ namespace RegistrationApp.Tests.DataAccess
 
             Assert.Equal(1, courses.Count);
             Assert.Equal("Intro to Computer Science", courses[0].Course.Title);
+        }
+
+        /// <summary>
+        /// Make sure we can add a Student to the student body.
+        /// </summary>
+        [Fact]
+        public void Test_AddStudent()
+        {
+            MockDatabase<Student> mockDB = new MockDatabase<Student>(c => c.Students);
+            RegistrationData data = new RegistrationData(mockDB.Context);
+
+            data.AddStudent(new Student());
+
+            mockDB.MockSet.Verify(m => m.Add(It.IsAny<Student>()), Times.Once());
+            mockDB.MockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        /// <summary>
+        /// Make sure we can remove a Student from the student body.
+        /// </summary>
+        [Fact]
+        public void Test_RemoveStudent()
+        {
+            MockDatabase<Student> mockDB = new MockDatabase<Student>(c => c.Students);
+            RegistrationData data = new RegistrationData(mockDB.Context);
+
+            Student student = new Student();
+
+            mockDB.AddDataEntry(student);
+
+            data.RemoveStudent(student);
+
+            mockDB.MockSet.Verify(m => m.Remove(It.IsAny<Student>()), Times.Once());
+            mockDB.MockContext.Verify(m => m.SaveChanges(), Times.Once());
         }
     }
 }
